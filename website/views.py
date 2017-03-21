164ca8d27911bbed4909509d9f93c8bf18816e57
howardrj <howardrj@tcd.ie>
2017-03-21 17:57:13 +0000

    Added complete contact form handling.

--- website/views.py
+++ website/views.py
@@ -4,7 +4,6 @@ import smtplib
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
-from django.conf import settings
 
 from website.forms import ContactForm
 
@@ -18,7 +17,6 @@ def indexPage(request):
 
     contact_form = ContactForm()
     email_fail   = False
-    contact_fail = False
 
     if request.method == 'POST' and 'contact' in request.POST:
 
@@ -31,25 +29,49 @@ def indexPage(request):
             logger.info("Contact form data was valid. Generating email")
 
             data = {}
-            data['name']    = contactForm.cleaned_data['name']
-            data['email']   = contactForm.cleaned_data['email']
-            data['subject'] = contactForm.cleaned_data['subject']
-            data['message'] = contactForm.cleaned_data['message']
+            data['name']    = contact_form.cleaned_data['name']
+            data['email']   = contact_form.cleaned_data['email']
+            data['subject'] = contact_form.cleaned_data['subject']
+            data['message'] = contact_form.cleaned_data['message']
 
             try:
                 contact_form.sendEmail(data)
 
-                return HttpResponseRedirect('/')
+                response = HttpResponseRedirect('/')
+
+                # Add cookie so we know succesful was posted when handling redirect
+                # Could pass this email to template (not doing it at the moment)
+                response.set_cookie('provided_email', data['email'])
+
+                return response
 
             except smtplib.SMTPException:
                 logger.error("Error occured sending contact email")
                 email_fail = True
         else:
-            contact_fail = True
             logger.error("Contact form data was invalid")
 
-    context_dict = {'contact_form': contact_form,
-                    'contact_fail': contact_fail,
-                    'email_fail'  : email_fail}
+    logger.info("Rendering index.html")
 
-    return render_to_response('index.html', context_dict, context)
+    # This means a successful contact form was posted
+    if request.COOKIES.get('provided_email'):
+
+        context_dict = {'contact_form': contact_form,
+                        'contacted'   : True,
+                        'email_fail'  : email_fail}
+
+        response = render_to_response('index.html', context_dict, context)
+        response.delete_cookie('provided_email')
+
+        return response
+
+    else:
+
+        context_dict = {'contact_form': contact_form,
+                        'contacted'   : False,
+                        'email_fail'  : email_fail}
+
+        if email_fail:
+            context_dict['email'] = data['email']
+
+        return render_to_response('index.html', context_dict, context)
