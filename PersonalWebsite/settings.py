eb9f9e0e7185a64402978dff36366dcb76002d30
howardrj <howardrj@tcd.ie>
2019-02-12 09:27:17 +0000

    Included about section and expanded portfolio.

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = secrets.KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.28.4.152', 'robbie-howard.com', '127.0.0.1']
 
 
 # Application definition
