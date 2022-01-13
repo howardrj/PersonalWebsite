dde424c9f8fdbfd422c8d5bf166dfe2b1aaa9d01
howardrj <howardrj@tcd.ie>
2022-01-13 01:16:25 +0000

    Added poem_per_day generator to start_services.sh

--- service_scripts/start_services.sh
+++ service_scripts/start_services.sh
@@ -5,10 +5,14 @@ echo "Starting all Personal Website services"
 cp /var/www/Personal-Website/service_scripts/PersonalWebsiteGunicorn.service /etc/systemd/system/
 cp /var/www/Personal-Website/service_scripts/PersonalWebsite /etc/nginx/sites-available/
 cp /var/www/Personal-Website/service_scripts/PersonalWebsite /etc/nginx/sites-enabled/
+cp /var/www/Personal-Website/poem_per_day/service_scripts/GeneratePoemPerDayGenerate.service /etc/systemd/system/
 
 systemctl daemon-reload
 
 systemctl enable PersonalWebsiteGunicorn.service
 systemctl restart PersonalWebsiteGunicorn.service
+
+systemctl enable GeneratePoemPerDayGenerate.service
+systemctl restart GeneratePoemPerDayGenerate.service
 
 systemctl restart nginx
