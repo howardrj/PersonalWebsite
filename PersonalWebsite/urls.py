9a7438a135f9c21dc8e657016e1e3c7ff1cc2fcf
howardrj <howardrj@tcd.ie>
2022-01-16 17:31:07 +0000

    Included restructured maze_generator repo

--- PersonalWebsite/urls.py
+++ PersonalWebsite/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from website.views import index_page
-from maze_generation.views import maze_generation_page
+from maze_generation import urls as maze_generation_urls
 from poem_per_day import urls as poem_per_day_urls
 
 urlpatterns = []
 
+urlpatterns.extend(maze_generation_urls.urlpatterns)
 urlpatterns.extend(poem_per_day_urls.urlpatterns)
 
-urlpatterns.append(url(r'^maze_generation/', maze_generation_page))
 urlpatterns.append(url(r'^$', index_page))
