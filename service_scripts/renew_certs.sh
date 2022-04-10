a8592cd5e5fcc5e7213ff187f9a080afe2fd61c4
howardrj <howardrj@tcd.ie>
2022-04-10 15:09:15 +0100

    Update renew_certs.sh

--- service_scripts/renew_certs.sh
+++ service_scripts/renew_certs.sh
@@ -1,17 +1,1 @@
-echo "Stopping nginx ..."
-systemctl stop nginx
-echo "Stopped nginx"
-
-echo "Renewing certificates ..."
-/var/www/letsencrypt/letsencrypt-auto --server https://acme-v01.api.letsencrypt.org/directory --standalone --expand -d robbie-howard.com -d www.robbie-howard.com certonly
-
-if [ $? -ne 0 ]
-then
-    echo "Successfully renewed certificates"
-else
-    echo "ERROR: Failed to renew certificates"
-fi
-
-echo "Restarting nginx ..."
-systemctl restart nginx
-echo "Restarted nginx"
+# Just use 'certbot' command and update certs interactively for now
